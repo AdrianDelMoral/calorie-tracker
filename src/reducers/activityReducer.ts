22f62be8a40013b21393ccb0ffac7bc2dd24d11f
Adrián Del Moral <adrian.delmoral.martin@gmail.com>
2024-09-29 20:18:05 +0200

    Adding the functionality to update the list activities

--- src/reducers/activityReducer.ts
+++ src/reducers/activityReducer.ts
@@ -23,11 +23,21 @@ export const activityReducer = (
     action: ActivityActions  // Le asignamos el ActivyActions para las acciones(Las acciones nos van a ayudar a describir qué es lo que está pasando y qué información es la que va a modificar qué parte de nuestro state.) // Una acciçon consta de dos partes: consta de dos partes el type, que es la descripción y el payload, que es la información que modifica o que vamos a agregar a nuestro state.
 ) => {
     if(action.type === 'save-activity'){ // Una vez que yo mande a llamar, la acción de arriba "save-activity" va a escanear nuestro código y va a buscar ese mismo type para ejecutar ese código.
+        
         // Este código maneja la lógica para actualizar el state
+        let updatedActivities : Activity[] = [] // generamos un state vacio, que en base a las siguientes acciones se rellena o no
+
+        if (state.activeId) { // si tenemos algo significa que estamos editando
+            // En caso de que estemos modificando, mapearemos el state de activities, y si coincide lo cambiará, y en caso contrario para no perder las demás retornaremos la actividad si no aparecerán NULL
+            updatedActivities = state.activities.map(activity => activity.id === state.activeId ? action.payload.newActivity : activity) 
+        } else {
+            updatedActivities = [...state.activities, action.payload.newActivity]
+        }
 
         return {
             ...state, // copia del state para asi tener todas las actividades
-            activities: [ ...state.activities,action.payload.newActivity ]
+            activities: updatedActivities, // actualiza con lo que tenga nuestro state en base a la acción anterior
+            activeId: '' // reiniciamos el activeId cada vez que el formulario le demos al "Guardar"
         }
     }
     if (action.type == 'set-activeId') {
